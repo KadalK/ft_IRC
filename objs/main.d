614e96a88087c90be71e487ddb55e0aa9c20f883
Erwann Gache <egache@z4r4p5.42lyon.fr>
2026-04-21 19:26:50 +0200

    Not much, but we understood A LOT

---
+++ objs/main.d
@@ -0,0 +1,12 @@
+objs/main.o: src/main.cpp include/Server.hpp include/Client.hpp \
+  include/Channel.hpp include/Commands.hpp include/PrivMsg.hpp
+
+include/Server.hpp:
+
+include/Client.hpp:
+
+include/Channel.hpp:
+
+include/Commands.hpp:
+
+include/PrivMsg.hpp:
