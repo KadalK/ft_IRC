47ad5b10a7230c29f2ff8fbf6dd85dd396950628
Theo Saby <tsaby@z4r6p4.42lyon.fr>
2026-04-22 11:04:07 +0200

    add class for execption and errno, and throw exception on init

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,21 +1,28 @@
 #include "Server.hpp"
+#include "SystemException.hpp"
 
-
-Server::Server(int port,std::string password) : _port(port) , _password(password)
-{}
+Server::Server(int port, std::string password) : _port(port), _password(password)
+{
+}
 
 void Server::init()
 {
 	int optval = 1;
 	this->_serverSocketFd = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(this->_serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+	if (this->_serverSocketFd < 0)
+		throw SystemException("Erreur de création du socket");
+	if (setsockopt(this->_serverSocketFd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
+		throw SystemException("Erreur de configuration du socket");
 	sockaddr_in serverAddress;
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(this->_port);
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
-	bind(this->_serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-	fcntl(this->_serverSocketFd, F_SETFL, O_NONBLOCK);
-	listen(this->_serverSocketFd, 5);
+	if (bind(this->_serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+		throw SystemException("Erreur d'attachement au port");
+	if (fcntl(this->_serverSocketFd, F_SETFL, O_NONBLOCK) < 0)
+		throw SystemException("Erreur lors du passage en mode non-bloquant");
+	if (listen(this->_serverSocketFd, 5) < 0)
+		throw SystemException("Impossible d'ouvrir les portes du serveur pour écouter");
 }
 
 void Server::createNewClient()
@@ -42,7 +49,6 @@ void Server::removeClient(int fd)
 	std::cout << "client remove " << this->_registry[fd]->getNickname() << " le fd etait : " << fd << std::endl;
 }
 
-
 void Server::handleClientData(int fd)
 {
 	char temp[1024] = {0};
@@ -58,39 +64,39 @@ void Server::handleClientData(int fd)
 			std::string command = this->_registry[fd]->getBuffer().substr(0, pos);
 			this->_registry[fd]->setBuffer(this->_registry[fd]->getBuffer().erase(0, pos + 2));
 			// envoie au parseur.
-			std::cout << "commande recu :" << command << " fd :" << fd  << "lenght : "<< command.length() << std::endl;
+			std::cout << "commande recu :" << command << " fd :" << fd << "lenght : " << command.length() << std::endl;
 		}
 	}
 }
 
 void Server::sendToClient(int fd)
 {
-	if ( this->_registry.find(fd) == this->_registry.end())
+	if (this->_registry.find(fd) == this->_registry.end())
 		return;
 	Client *client = this->_registry[fd];
 	std::string message = client->getBufferOut();
 	if (message.empty())
 		return;
-	int bytesSent = send(fd,message.c_str(),message.length(),MSG_NOSIGNAL);
+	int bytesSent = send(fd, message.c_str(), message.length(), MSG_NOSIGNAL);
 	if (!bytesSent)
 	{
 		this->removeClient(fd);
 		return;
 	}
-	client->setBufferOut(message.erase(0,bytesSent));
-	if(client->getBufferOut().empty())
+	client->setBufferOut(message.erase(0, bytesSent));
+	if (client->getBufferOut().empty())
 	{
 		epoll_event event;
 		event.events = EPOLLIN;
 		event.data.fd = fd;
-		epoll_ctl(this->_epollFd, EPOLL_CTL_MOD, fd,&event);
+		epoll_ctl(this->_epollFd, EPOLL_CTL_MOD, fd, &event);
 	}
 }
 
-Client* Server::getClientByNickname(std::string nickname)
+Client *Server::getClientByNickname(std::string nickname)
 {
-	std::map<int,Client*>::iterator it = this->_registry.begin();
-	while(it != this->_registry.end())
+	std::map<int, Client *>::iterator it = this->_registry.begin();
+	while (it != this->_registry.end())
 	{
 		if (it->second->getNickname() == nickname)
 			return (it->second);
@@ -104,7 +110,7 @@ void Server::clientWrite(int fd)
 	epoll_event event;
 	event.events = EPOLLIN | EPOLLOUT;
 	event.data.fd = fd;
-	epoll_ctl(this->_epollFd,EPOLL_CTL_MOD,fd,&event);
+	epoll_ctl(this->_epollFd, EPOLL_CTL_MOD, fd, &event);
 }
 
 void Server::run()
@@ -138,14 +144,14 @@ void Server::run()
 
 Server::~Server()
 {
-	std::map<int,Client*>::iterator it = this->_registry.begin();
-	while(it != this->_registry.end())
+	std::map<int, Client *>::iterator it = this->_registry.begin();
+	while (it != this->_registry.end())
 	{
 		delete it->second;
 		close(it->first);
 	}
 	if (this->_serverSocketFd != -1)
-        close(this->_serverSocketFd);
-    if (this->_epollFd != -1)
-        close(this->_epollFd);
+		close(this->_serverSocketFd);
+	if (this->_epollFd != -1)
+		close(this->_epollFd);
 }
