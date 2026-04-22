aae0955d3da6b09b02e421f05666decefd26fb19
Erwann Gache <egache@z4r4p6.42lyon.fr>
2026-04-22 18:16:21 +0200

    pre commit before changes

--- src/PreParser.cpp
+++ src/PreParser.cpp
@@ -14,9 +14,14 @@ std::vector<std::string> ft_tokenizer(std::string rawCommand)
 
   for (i = 0; i < rawCommand.size(); i++)
   {
-    if (rawCommand[i] == ' ')
+    if (rawCommand[i] == ':')
     {
-      if (!rawCommand.substr(start, i - start).empty())
+      tokens.push_back(rawCommand.substr(start));
+      return (tokens);
+    }
+    else if (rawCommand[i] == ' ')
+    {
+       if (!rawCommand.substr(start, i - start).empty())
         tokens.push_back(rawCommand.substr(start, i - start));
       while (rawCommand[i] == ' ')
         i++;
@@ -30,12 +35,11 @@ std::vector<std::string> ft_tokenizer(std::string rawCommand)
 
 void parser(std::string rawMessage)
 {
-  PrivMsg p;
   std::vector<std::string> v;
-  std::map<std::string, *Commands> commands = {{"PRIVMSG", p}}; //add toutes les commandes
+  std::map<std::string, std::string> commands = {{"PRIVMSG", "je suis un message prive"}, {"JOIN", "je suis un join"}}; //add toutes les commandes
 
   size_t pos;
-  std::map<std::string, *Commands>::iterator i;
+  std::map<std::string, std::string>::iterator i;
 
   pos = rawMessage.find(' ');
   if (pos == std::string::npos)
@@ -47,30 +51,17 @@ void parser(std::string rawMessage)
   while (rawMessage[pos] == ' ')
     pos++;
   v = ft_tokenizer(rawMessage.substr(pos));
-  i->second->execute(v);
+  for (std::vector<std::string>::iterator it = v.begin(); it != v.end(); it++)
+    std::cout << "[" << *it << "]" << std::endl;
+  std::cout << i->second << std::endl;
   return;
 }
 
-// void parser(std::string rawMessage)
-// {
-//   std::vector<std::string> v;
-//   std::map<std::string, std::string> commands = {{"PRIVMSG", "je suis un message prive"}, {"JOIN", "je suis un join"}}; //add toutes les commandes
-//
-//   size_t pos;
-//   std::map<std::string, std::string>::iterator i;
-//
-//   pos = rawMessage.find(' ');
-//   if (pos == std::string::npos)
-//     return; // Manque arguments - throw exception
-//   std::cout << "Commande : " << rawMessage.substr(0, pos) << std::endl;
-//   i = commands.find(rawMessage.substr(0, pos));
-//   if (i == commands.end())
-//     return; //Commande existe pas - Throw exception
-//   while (rawMessage[pos] == ' ')
-//     pos++;
-//   v = ft_tokenizer(rawMessage.substr(pos));
-//   for (std::vector<std::string>::iterator it = v.begin(); it != v.end(); it++)
-//     std::cout << "[" << *it << "]" << std::endl;
-//   std::cout << i->second << std::endl;
-//   return;
-// }
+int main()
+{
+  std::string str = "PRIVMSG salut,ca,va :coucou bon";
+  parser(str);
+  str = "PRIVMSG salut,ca,va coucou bon";
+  parser(str);
+  return 0;
+}
