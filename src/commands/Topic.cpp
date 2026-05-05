d424a879ee3135a063150b62beaa2b2a66f00be9
kapinarc <89167555+KadalK@users.noreply.github.com>
2026-05-05 16:48:26 +0200

    ervann clc 2

--- src/commands/Topic.cpp
+++ src/commands/Topic.cpp
@@ -7,22 +7,6 @@ void Topic::parsingMessage(std::string rawText){
 	(void)rawText;
 }
 
-// static std::vector<std::string> extractTokens(const std::string &str)
-// {
-// 	std::vector<std::string> v;
-// 	size_t start = 0;
-// 	size_t pos;
-// 	while ((pos = str.find(",", start)) != std::string::npos)
-// 	{
-// 		if (!str.substr(start, pos - start).empty())
-// 			v.push_back(str.substr(start, pos - start));
-// 		start = pos + 1;
-// 	}
-// 	if (!str.substr(start).empty())
-// 		v.push_back(str.substr(start));
-// 	return (v);
-// }
-
 void Topic::execute(Client& client, ClientHandler &clH, ChannelHandler &chH, const std::vector<std::string>& arg){
 	if (arg.size() < 2)
 		return;
